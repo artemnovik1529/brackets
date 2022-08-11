4408e3f2dec586c9806571cd035d4b4bfe9475aa
artem <mr.artnov@mail.ru>
2022-08-12 00:13:37 +0300

    add brackets

--- src/index.js
+++ src/index.js
@@ -1,87 +1,31 @@
-module.exports =
-    function check(str, bracketsConfig) {
-    if (str.length % 2 !== 0 ) {
-        return false;
-    }
-
-    let configs = {};
-    let openBrackets = [];
-    let closeBrackets = [];
-    let sameBrackets = {};
-    let stack = [];
-    let result = true;
-    let positionCounter = 0;
-
-    bracketsConfig.forEach(function (config, ind, arr) {
-        configs[config.join('')] = {
-            open: config[0],
-            close: config[1],
-        }
-        openBrackets.push(config[0]);
-        closeBrackets.push(config[1]);
-        if (config[0] === config[1]) {
-            sameBrackets[config.join('')] = {
-                open: config[0],
-                close: config[1],
-            }
-        }
-    });
-
-    if (Object.keys(sameBrackets).length) {
-        for (const [key, value] of Object.entries(sameBrackets)) {
-            if (value.open === '|') {
-                value.open = '\\|';
-            }
-            let regexp = new RegExp(value.open, 'g');
-
-            if ((str.match(regexp) || []).length % 2 === 0) {
-                str = str.replace(regexp, '');
+module.exports = function check(str, bracketsConfig) {
+    let result = '',
+        objects = {};
+    for (let i=0;i<=bracketsConfig.length-1;i++) {
+        objects[bracketsConfig[i][0]] = bracketsConfig[i][1];
+    };
+    for (let b=0;b<=str.length-1;b++) {
+        if (str[b] === objects[str[b]]) {
+            if (result[result.length-1] === objects[str[b]]) {
+                result = result.substring(0, result.length-1);
             } else {
-                return false;
+                result += str[b];
+            }
+        } else {
+            if (Object.keys(objects).includes(str[b])) {
+                result += str[b];
+            } else {
+                if(str[b] === objects[result[result.length-1]] ) {
+                    result = result.substring(0, result.length-1);
+                } else {
+                    return false
+                }
             }
         }
-    }
-    console.log(str);
-    str.split('').forEach(function (item, ind, arr) {
-        if (isOpenBrackets(item, openBrackets)) {
-            let i = getCloseBracket(item, configs);
-            stack.push(i);
-            return;
-        }
-
-        let prevItem = stack.pop();
-        if (item === prevItem) {
-            return;
-        } else {
-            result = false;
-        }
-    });
-
-    return result && stack.length === 0;
-
-
-    function isOpenBrackets(bracket, openBrackets) {
-        return openBrackets.includes(bracket);
-    }
-
-    function getCloseBracket(openBracket, configs) {
-        let result = null;
-        for (const [key,value] of Object.entries(configs)) {
-            if (key.includes(openBracket)) {
-                result = value.close;
-            };
-        }
-
-        return result;
+    };
+    if (result.length === 0) {
+        return true
+    } else {
+        return false
     }
 }
-//
-// const config1 = [['(', ')']];
-// const config2 = [['(', ')'], ['[', ']']];
-// const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
-// const config4 = [['|', '|']];
-// const config5 = [['(', ')'], ['|', '|']];
-// const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
-// const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
-// console.log(check('|(|)', config5));
-// console.log(check('5555512575557777777555566667888888667661133833448441111222233333444442266666', config6));
